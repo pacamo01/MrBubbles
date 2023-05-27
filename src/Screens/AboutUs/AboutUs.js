afdd8f358c5a45033dfa11384acdb13e7e5266a9
pacamo01 <20210022674@my.xu.edu.ph>
2023-05-27 23:09:22 +0800

    About us layout

--- src/Screens/AboutUs/AboutUs.js
+++ src/Screens/AboutUs/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
+import logo from '../../Assets/logo.png';
 import './AboutUs.css';
 
 function AboutUs() {
@@ -7,8 +8,12 @@ function AboutUs() {
     <div className="about-container">
     <Header/>
         <div className="container">
-            <h1 style={{marginTop:'120px'}}>Welcome to the Plain Page</h1>
-            <p>This is a plain page component.</p>
+            <div style={{backgroundColor: '#09606c', marginTop: '100px', width: '1000px', height: '500px'}}>
+                <img style={{width: '120px', height: '120px', marginLeft: '450px', marginTop: '20px'}} src={logo} className="logo" alt="logo" />
+                <div style={{backgroundColor: 'white', width: '90%', height: '56%', marginLeft: '5%'}}>
+                <p style={{fontSize: '25px', marginBottom: '100px'}}>Welcome to Mr. Bubbles, where cleanliness meets convenience. We provide top-quality laundry services tailored to fit your busy lifestyle. With state-of-the-art equipment and skilled staff, we handle your garments with care and precision. From regular washing to delicate care and stain removal, we've got you covered. Enjoy prompt service and customer satisfaction at Mr. Bubbles. Experience the joy of fresh, perfectly laundered clothes today.</p>
+                </div>
+            </div>
       </div>
     </div>
   );
--- src/Screens/ContactUs/ContactUS.js
+++ src/Screens/ContactUs/ContactUS.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
+import logo from '../../Assets/logo.png';
 import './ContactUs.css';
 
 function ContactUs() {
@@ -7,8 +8,14 @@ function ContactUs() {
     <div className="contact-container">
     <Header/>
         <div className="container">
+          <div>
+          <div className="contact-logo-container">
+                  <img src={logo} className="logo" alt="logo" />
+                  <h2 className="heading">Mr. Bubbles</h2>
+                </div>
             <h1 style={{marginTop:'120px'}}>Welcome to the Plain Page</h1>
             <p>This is a plain page component.</p>
+          </div>
       </div>
     </div>
   );
